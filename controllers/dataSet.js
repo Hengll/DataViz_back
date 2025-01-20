ff9a6ffbcbb2b75a3cbd5238f441ef548969ef92
Hengll <heeric891120@gmail.com>
2025-01-20 16:39:28 +0800

    更新

--- controllers/dataSet.js
+++ controllers/dataSet.js
@@ -1,11 +1,11 @@
-import dataSet from '../models/dataSet.js'
+import DataSet from '../models/dataSet.js'
 import { StatusCodes } from 'http-status-codes'
 
 export const create = async (req, res) => {
   try {
     req.body.user = req.user._id
 
-    await dataSet.create(req.body)
+    await DataSet.create(req.body)
     res.status(StatusCodes.OK).json({
       success: true,
       message: '',
@@ -31,7 +31,27 @@ export const create = async (req, res) => {
 
 export const getDataName = async (req, res) => {
   try {
-    const result = await dataSet.find({ user: req.user._id }, 'dataName')
+    const result = await DataSet.find({ user: req.user._id }, 'dataName')
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result,
+    })
+  } catch (err) {
+    console.log('err : controllers/dataSet.js\n', err)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: 'serverError',
+    })
+  }
+}
+
+export const getDataNameById = async (req, res) => {
+  try {
+    const result = await DataSet.find({ user: req.user._id, _id: req.params.id }).orFail(
+      'NOT_FOUND',
+    )
 
     res.status(StatusCodes.OK).json({
       success: true,
--- routers/dataSet.js
+++ routers/dataSet.js
@@ -6,5 +6,6 @@ const router = Router()
 
 router.post('/', auth.jwt, dataSet.create)
 router.get('/getDataName', auth.jwt, dataSet.getDataName)
+router.get('/getDataName/:id', auth.jwt, dataSet.getDataNameById)
 
 export default router
